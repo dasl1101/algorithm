5f6e5d419a6d27224ace434ca48bd77dc49a6f1b
dasl1101 <ektmf1101@gmail.com>
2022-08-26 22:13:25 +0900

    [update]220826_거스름돈

--- 이코테/220826_거스름돈.java
+++ 이코테/220826_거스름돈.java
@@ -1,26 +1,15 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-//백준 1292 : 20220823
-
+//이코테 20220826 - 거스름돈
 public class Main {
 	public static void main(String[] args) throws Exception{
-		ArrayList<Integer> arr = new ArrayList<Integer>(); 
 		Scanner in = new Scanner(System.in);
-		int A = in.nextInt();
-		int B = in.nextInt();
-		int sum = 0;
-		
-		for(int i = 1; i <= B; i++) {
-			for(int j = 0; j < i; j++) {
-				if(arr.size() == B) {
-					break;
-				}
-				arr.add(i);
-			}
+		int[] coin =  {500, 100, 50, 10}; //동전의 종류
+		int N = in.nextInt(); //받는 돈
+		int count = 0; //동전 개수
+		for(int i=0; i<coin.length; i++) {
+			count+= N / coin[i]; //큰 수부터 나눈 값을 동전 개수에 더한다
+			N %= coin[i]; //나머지를 다시 N에 대입한다
 		}
-		for(int i = A-1; i <= B-1; i++) {
-			sum = sum + arr.get(i);
-		}
-		System.out.println(sum);
+		System.out.println(count);
 	}
 }
